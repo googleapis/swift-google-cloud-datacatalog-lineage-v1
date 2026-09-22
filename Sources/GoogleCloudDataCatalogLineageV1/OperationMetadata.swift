dbf64f32de238e7b232ea1ad5d682a47fedd1067
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:42:28 -0400

    chore: update librarian w/ better enum docs (#1017)
    In this version of librarian we improve the generated documentation for
    `enum` so the possible evolution and usage of the enums is more obvious.
    For example, we recommend using `@unknown default:` so they get warnings
    when new values appear in the enum.

--- Sources/GoogleCloudDataCatalogLineageV1/OperationMetadata.swift
+++ Sources/GoogleCloudDataCatalogLineageV1/OperationMetadata.swift
@@ -121,6 +121,12 @@ public struct OperationMetadata: Codable, Equatable, GoogleWKT._AnyPackable,
   }
 
   /// An enum with the state of the operation.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum State: Codable, Equatable, Sendable {
     /// Unused.
     case unspecified
@@ -134,15 +140,21 @@ public struct OperationMetadata: Codable, Equatable, GoogleWKT._AnyPackable,
     case failed
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
@@ -240,6 +252,12 @@ public struct OperationMetadata: Codable, Equatable, GoogleWKT._AnyPackable,
   }
 
   /// Type of the long running operation.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum Type_: Codable, Equatable, Sendable {
     /// Unused.
     case unspecified
@@ -249,15 +267,21 @@ public struct OperationMetadata: Codable, Equatable, GoogleWKT._AnyPackable,
     case create
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
--- Sources/GoogleCloudDataCatalogLineageV1/SearchLineageStreamingRequest.swift
+++ Sources/GoogleCloudDataCatalogLineageV1/SearchLineageStreamingRequest.swift
@@ -407,6 +407,12 @@ public struct SearchLineageStreamingRequest: Codable, Equatable, GoogleWKT._AnyP
   }
 
   /// Direction of the search.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum SearchDirection: Codable, Equatable, Sendable {
     /// Direction is unspecified.
     case unspecified
@@ -416,15 +422,21 @@ public struct SearchLineageStreamingRequest: Codable, Equatable, GoogleWKT._AnyP
     case upstream
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
@@ -512,6 +524,12 @@ public struct SearchLineageStreamingRequest: Codable, Equatable, GoogleWKT._AnyP
   }
 
   /// Entity set restriction.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum EntitySet: Codable, Equatable, Sendable {
     /// The entity set is unspecified. Returns all the data.
     case unspecified
@@ -520,15 +538,21 @@ public struct SearchLineageStreamingRequest: Codable, Equatable, GoogleWKT._AnyP
     case entities
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
